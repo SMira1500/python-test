f6fc26c2fe8a235645454b94a2d6edfb8f757386
SMira1500 <quitaser@gmail.com>
2024-01-08 19:46:46 -0600

    tutorial bitch

---
+++ sophia.py
@@ -0,0 +1,42 @@
+cond_1 = True
+cond_2 = False
+
+if cond_1:
+  print("cond_1 is True")
+else:
+  print("cond_1 is False")
+  
+print("cond_2 is False or True")
+
+cond_3 = 1
+
+if cond_3 == 3:
+  print("cond_3 is 3")
+elif cond_3 == 2:
+  print("cond_3 is 2")
+else:
+  print("cond_3 is not 2 or 3")
+
+'''
+if cond_3 has the value fo 3, then we will print "cond_3 is 3"
+if cond_3 doe snot vlaue of 3 but has the value of 2, then we will print "cond_3 is 2"
+'''
+
+# 3 times 5
+
+product1 = 3
+product2 = 5
+result = 0
+
+for i in range(0, product2):
+  print('i: ' + str(i))
+  result = result + product1
+  
+print('result: ' + str(result))
+# result: 15
+  
+'''
+startIndex, endIndex
+yo mama
+[0, 1, 2, 3, ..., product2]
+'''
